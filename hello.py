47238a23c583f0227c11b640f22a78796c239a8b
KaveriMetri07 <kaverimetri07@gmail.com>
2025-03-17 15:56:56 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("hello, world!")
+# name=input("Name: ")
+# print("Hello , "+name)
+# print(f"hello, {name}")
+
+
+# condition in python
+
+n=int(input("Number: "))
+
+if n>0:
+    print("Number is positive")
+elif n<0:
+    print("Number is negative")
+else:
+    print("Number is zero")
