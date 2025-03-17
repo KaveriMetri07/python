d5a35295b35c18a2e2daaf6a8f1fe035ca9dfbb7
KaveriMetri07 <kaverimetri07@gmail.com>
2025-03-17 15:58:15 +0530

    sequence.py added

---
+++ sequences.py
@@ -0,0 +1,25 @@
+#list are mutable values and are indicated using the square brackets[]
+# name=["zavier","kaveri","aiden","vijay"]
+# print(name)
+# name.append("kashava")
+# print(name)
+# name.sort()
+# print(name)
+
+
+#sets are unique values 
+s=set()#empty set is created
+#add elements to sets s
+s.add(10)
+s.add(20)
+s.add(30)
+s.add(40)
+s.remove(20)
+s.add(10)
+s.add(50)
+
+
+
+# s.remove(10)
+print(s)
+print(f"the set s contains {len(s)} elements {s}")
